ddf249646d70c229efe19f657ebe2d9c425c2fde
ricfrank <riccardo.franconi@gmail.com>
2015-02-17 22:13:54 +0100

    remove unused stuff to install ansible

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 
 sudo apt-get update
+
+printf '**************************\n\n'
+printf 'Install cachefilesd'
+printf '**************************\n\n'
 # NFS improve caching
 sudo apt-get install cachefilesd
 sudo echo "RUN=yes" > /etc/default/cachefilesd
 sudo service cachefilesd start
+printf '**************************\n\n'
+printf 'Finish install  cachefilesd'
+printf '**************************\n\n'
+
+printf '**************************\n\n'
+printf 'Install ansible'
+printf '**************************\n\n'
 sudo apt-get install -y python-pip python-dev python-pycurl
 sudo pip install ansible
 
 sudo mkdir -p /etc/ansible
-#printf '**************************\n\n'
-#printf '[vagrant]\nlocalhost\n' | sudo tee /etc/ansible/hosts > /dev/null
-#printf '**************************\n\n'
-
-if [[ ! -d /.vm-stuff ]]; then
-    mkdir /.vm-stuff
-    printf '**************************\n\n'
-    printf "Created directory /.vm-stuff"
-    printf '**************************\n\n'
-fi
-
-if [[ ! -f /.vm-stuff/add-ssh-key ]]; then
-    printf '**************************\n\n'
-    printf 'ssh key gen\n'
-    printf '**************************\n\n'
-
-    ssh-keygen -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
-    touch /.vm-stuff/add-ssh-key
-fi
 
 printf '**************************\n\n'
 printf 'Removes executable permission on hosts.ini to avoid ansible evalute this inventory like external scritp'
 chmod -x /var/www/provisioning/hosts.ini
 printf '**************************\n\n'
 
-
 printf '**************************\n\n'
-printf 'Finish controlhost provisioning'
+printf 'Finish install ansible'
 printf '**************************\n\n'
