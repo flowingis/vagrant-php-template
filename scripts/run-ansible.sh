f9e14a8cd1b68b7980eba5e6dca6e4fe9e0ce6e2
alemazz <alessandromazzoli91@gmail.com>
2015-08-24 18:28:56 +0200

    add layout for deployment pipeline, configured modules for production-like environment,also enabled CentOS deployment

--- scripts/run-ansible.sh
+++ scripts/run-ansible.sh
@@ -4,5 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 export PYTHONUNBUFFERED=1
 
 echo Running ansible playbooks as local
-ansible-playbook $1/provisioning/playbooks.yml -i $1/provisioning/hosts.ini --extra-vars="target=development" --connection=local
+ansible-playbook $1/provisioning/playbooks.yml -i $1/provisioning/hosts.ini --extra-vars="target=development target_user=vagrant" --connection=local
 
