efa64b93dbfea3d4e8b2d7d58f4d30350fdf352f
alemazz <am@ideato.it>
2015-08-27 15:32:57 +0200

    deleted:    scripts/install-ansible-centos.sh 	deleted:    scripts/run-ansible-centos.sh

--- scripts/run-ansible-centos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo Running ansible playbooks as local on CentOS
-ansible-playbook $1/provisioning/playbooks.yml -i $1/provisioning/hosts.ini --extra-vars="target=staging target_user=vagrant" --connection=local
-
