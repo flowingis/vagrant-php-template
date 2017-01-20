d020ca50db2278dd4830699537bb098ca45544a6
ricfrank <riccardo@MacBook-Pro-di-Riccardo-2.local>
2017-01-20 12:41:53 +0100

    move cachefilesd installation in ansible playbook and remove useless package

--- vagrant/scripts/install-ansible.sh
+++ vagrant/scripts/install-ansible.sh
@@ -3,28 +3,13 @@
 export DEBIAN_FRONTEND=noninteractive
 export PYTHONUNBUFFERED=1
 
+yum install -y epel-release
 sudo yum check-update
-
-printf '**************************\n\n'
-printf 'Install cachefilesd'
-printf '**************************\n\n'
-# NFS improve caching
-sudo yum -y install cachefilesd
-sudo echo "RUN=yes" > /etc/default/cachefilesd
-sudo /sbin/service cachefilesd start
-printf '**************************\n\n'
-printf 'Finish install  cachefilesd'
-printf '**************************\n\n'
 
 printf '**************************\n\n'
 printf 'Install ansible'
 printf '**************************\n\n'
-sudo yum install -y python-pip python-dev python-pycurl
-sudo yum check-update
-if ! rpm -qa | grep -qw epel-release-7-8.noarch; then
-    sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
-fi
-sudo yum -y check-update
+
 sudo yum -y install ansible
 
 sudo mkdir -p /etc/ansible
