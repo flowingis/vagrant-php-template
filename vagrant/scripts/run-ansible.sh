b72037a44c544582fddf2526aba05b9f4f7e702e
Marco Colombo <marco.colombo@ideato.info>
2017-04-14 17:05:45 +0200

    refactoring run-ansible.sh

--- vagrant/scripts/run-ansible.sh
+++ vagrant/scripts/run-ansible.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive
-export PYTHONUNBUFFERED=1
-
 echo Running ansible playbooks as local
 ansible-playbook $1/vagrant/provisioning/playbooks.yml -i $1/vagrant/provisioning/hosts.ini --extra-vars "synced_folder=$1 host=$2" --connection=local
