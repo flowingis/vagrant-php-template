c4241fdc3681db41388c51fd475788b1c8985470
Marco Colombo <marco.colombo@ideato.info>
2017-04-14 16:59:17 +0200

    refactoring install-ansible.sh

--- vagrant/scripts/install-ansible.sh
+++ vagrant/scripts/install-ansible.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive
-export PYTHONUNBUFFERED=1
+sudo yum install -y epel-release >/dev/null 2>&1
+sudo yum install -y python-pip python-dev python-pycurl >/dev/null 2>&1
+sudo yum install -y ansible >/dev/null 2>&1
+sudo mkdir -p /etc/ansible >/dev/null 2>&1
 
-yum install -y epel-release
-sudo yum check-update
-
-printf '**************************\n\n'
-printf 'Install ansible'
-printf '**************************\n\n'
-
-sudo yum -y install ansible
-
-sudo mkdir -p /etc/ansible
-
-printf '**************************\n\n'
-printf 'Removes executable permission on hosts.ini to avoid ansible evalute this inventory like external script'
-chmod -x $1/provisioning/hosts.ini
-printf '**************************\n\n'
-
-printf '**************************\n\n'
-printf 'Finish install ansible'
-printf '**************************\n\n'
