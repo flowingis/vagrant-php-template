fab73f382fe40d20b766f57c9fd2272d14ffcce7
Marco Colombo <marco.colombo@ideato.info>
2017-04-28 16:40:32 +0200

    add support for multi-env

--- vagrant/scripts/run-ansible.sh
+++ vagrant/scripts/run-ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo Running ansible playbooks as local
-ansible-playbook $1/vagrant/provisioning/playbooks.yml -i $1/vagrant/provisioning/hosts.ini --extra-vars "synced_folder=$1 host=$2" --connection=local
+ansible-playbook $1/vagrant/provisioning/playbooks.yml -i $1/vagrant/provisioning/env/dev/hosts.ini --extra-vars "synced_folder=$1 host=$2 env=dev" --connection=local
