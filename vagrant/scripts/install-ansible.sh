aded2b06a9e1b27ffa8866e4b4d0ffb83ea24c3b
Marco <marco.colombo@ideato.it>
2017-05-08 11:34:25 +0200

    removed unused vars

--- vagrant/scripts/install-ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo yum install -y epel-release >/dev/null 2>&1
-sudo yum install -y python-pip python-dev python-pycurl >/dev/null 2>&1
-sudo yum install -y ansible >/dev/null 2>&1
-sudo mkdir -p /etc/ansible >/dev/null 2>&1
-
